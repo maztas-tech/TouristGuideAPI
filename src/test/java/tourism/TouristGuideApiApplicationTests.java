594bb765db6916a8494e6b6960cf13714a2d3321
maztas-tech <mata0001@stud.kea.dk>
2024-02-05 12:48:02 +0100

    Initial commit

---
+++ src/test/java/tourism/TouristGuideApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
