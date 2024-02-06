392231ea36ab5cf6150cbd10b3e9a942b564e885
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-02-06 21:13:06 +0100

    1 Endpoint Get touristlist

--- src/main/java/tourism/controller/TouristController.java
+++ src/main/java/tourism/controller/TouristController.java
@@ -1,13 +1,32 @@
 package tourism.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tourism.model.TouristAttraction;
+import tourism.service.TouristService;
+
+import java.util.List;
 
 @Controller
-@RequestMapping(path = "attractions")
+@RequestMapping(path = "attraction")
 public class TouristController {
+
+    TouristService touristService;
+    public TouristController(){
+        this.touristService = new TouristService();
+    }
+
+    @GetMapping(path = "attractions")
+    public ResponseEntity<List<TouristService>> getTouristService(){
+        List touristList = touristService.getTouristAttractionArrayListService();
+
+        return new ResponseEntity<List<TouristService>>(touristList, HttpStatus.OK);
+    }
+
 
 
 
--- src/main/java/tourism/service/TouristService.java
+++ src/main/java/tourism/service/TouristService.java
@@ -1,7 +1,17 @@
 package tourism.service;
 
 import org.springframework.stereotype.Service;
+import tourism.model.TouristAttraction;
+import tourism.repository.TouristRepository;
+
+import java.util.List;
 
 @Service
 public class TouristService {
+
+    TouristRepository touristRepository = new TouristRepository();
+
+    public List<TouristAttraction> getTouristAttractionArrayListService(){
+        return touristRepository.getTouristAttractionArrayList();
+    }
 }
